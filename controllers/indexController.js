dedf2f43d4913fba2748fbd3fc00152c1482e055
Belen Medina Roldan <bmedinaroldan@gmail.com>
2020-12-03 11:32:45 -0300

    Se modifica Index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,13 +1,8 @@
 module.exports = {
-    index: (req, res) => {
+    home: (req, res) => {
         res.render('index', {
             title: 'Digital Heroes',
             text: 'Ni Superman, Iron Man o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y hueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan como inspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer en ti!.'});
         
-    },
-    credits: (req,res) => {
-        res.render('index', {
-            title: 'Digital Heroes',
-            text: 'Creditos <br> Ejercicio realizado por Belén Medina Roldán'});
     }
 }
